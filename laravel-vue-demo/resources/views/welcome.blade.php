d02090c2fd40fbd4497fbd80e381a3fef10f368b
mahmudul007 <sayedmahmudul05@gmail.com>
2023-08-19 11:33:23 +0600

    'ss'

---
+++ laravel-vue-demo/resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel With Vue</title>
+
+        @vite('resources/css/app.css')
+</head>
+<body>
+  <div id="app"></div>
+
+    @vite('resources/js/app.js')
+</body>
+</html>
